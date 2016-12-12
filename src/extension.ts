6489d63b3dd310e100aa34988cd769ad5d40be67
jitcoder <sam@bytecodestudios.com>
2016-12-11 22:08:18 -0800

    added RFC2616 parser, improved parsing, removed use of previewHtml to gain formating

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,11 @@
 import * as vscode from 'vscode';
 import * as request from 'request';
 import ElasticSearchResultsProvider from './results-provider';
+import url = require('url');
 
 export function activate(context: vscode.ExtensionContext) {
-  context.workspaceState.get("esQuery.host", null) || setElasticSearchHost(context);
-
-  let queryResultsUri = vscode.Uri.parse("es-query://results");
   let resultsProvider = new ElasticSearchResultsProvider();
+  const registration = vscode.workspace.registerTextDocumentContentProvider("es-query", resultsProvider);
 
   let disposable = vscode.commands.registerCommand('esQuery.execute', () => {
     const editor = vscode.window.activeTextEditor;
@@ -16,10 +15,10 @@ export function activate(context: vscode.ExtensionContext) {
 
     const range = selection.with(selection.start, selection.end);
     const selectedCode = editor.document.getText(range);
-    executeQuery(selectedCode, context);
+    executeQuery(selectedCode, context, resultsProvider);
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable, registration);
 }
 
 function getLines(code: string):string[] {
@@ -42,34 +41,84 @@ async function setElasticSearchHost(context: vscode.ExtensionContext) {
   context.workspaceState.update("esQuery.host", host);
 }
 
-function executeQuery(code:string, context: vscode.ExtensionContext):void {
-  const lines = getLines(code);
-  const host = context.workspaceState.get("esQuery.host", null);
-  let method:string|null = null;
-  let uri:string|null = null;
-  let body = '';
+interface ElasticSearchQuery {
+  method: string,
+  path: string,
+  body: Object
+}
 
-  for (let line of lines) {
-    if (!method && line.indexOf(' ') > -1) {
-      method = line.substring(0, line.indexOf(' ')).toUpperCase();
-      uri = line.substring(method.length, line.length);
+function parseSearchQuery(code: string): ElasticSearchQuery | null {
+  const requestReg = /^(GET|POST|DELETE|PUT|OPTIONS|PATCH|HEAD)\s?(\S+)?\s?(HTTP\/\d\.\d+)?$/gim;
+  const headerReg = /^([A-Za-z\-]+):(.+)$/gm;
+
+  if (!code.trim()) {
+    return null;
+  }
+  
+  let matches = requestReg.exec(code);
+  if (matches && matches.length > 1) {
+    const method: string = matches[1].toUpperCase();
+    const path: string = matches[2] || '';
+    const headers = {};
+    while ((matches = headerReg.exec(code)) !== null) {
+      headers[matches[1]] = matches[2];
+    }
+    
+    const jsonBodyStart: number = code.indexOf('{');
+    let body: Object;
+
+    if (jsonBodyStart > -1) {
+      try {
+        body = JSON.parse(code.substring(jsonBodyStart));
+      } catch (e) {
+        body = null
+      }
     }
 
-    body = `${body}\n${line}`
+    return <ElasticSearchQuery>{ method, path, body };
+  }
+  return null;
+}
+
+async function executeQuery(code:string, context: vscode.ExtensionContext, resultsProvider: ElasticSearchResultsProvider) {
+  const lines = getLines(code);
+  const host: string = context.workspaceState.get("esQuery.host", null) || await setElasticSearchHost(context);
+  const query: ElasticSearchQuery = parseSearchQuery(code);
+
+  if (!query) {
+    vscode.window.showErrorMessage("Unable to parse query");
+    return;
   }
 
-  method = method || "GET";
-  const json = JSON.parse(body);
+  const requestUrl: string = url.format({
+    host,
+    pathname: query.path,
+    protocol: 'http'
+  })
 
   request(<request.UrlOptions & request.CoreOptions>{
-    url: `http://${host}/${uri}`,
-    method,
-    json
+    url: requestUrl,
+    method: query.method,
+    json: query.body
   }, (error, response, body) => {
     if (error) {
       vscode.window.showErrorMessage(error);
     } else {
-      vscode.window.activeTextEditor.
+      let results = body;
+      if (typeof body === "string" && body.startsWith('{')) {
+        results = JSON.stringify(JSON.parse(body), null, 2);
+      } else if (typeof body === "string" && body.indexOf('\n')) {
+        results = results.replace(/\n/g, "\n").replace(/\r/g, "\r");
+      } else if (typeof body === "object") {
+        results = JSON.stringify(body, null, 2);
+      } else {
+        results = body;
+      }
+
+      resultsProvider.update(results);
+      // vscode.commands.executeCommand("vscode.previewHtml", resultsProvider.queryResultsUri, vscode.ViewColumn.Two, 'ElasticSearch Results');
+      vscode.workspace.openTextDocument(resultsProvider.queryResultsUri)
+        .then(doc =>  vscode.window.showTextDocument(doc, vscode.ViewColumn.Two, true));
     }
   })
 }
--- src/results-provider.ts
+++ src/results-provider.ts
@@ -1,18 +1,31 @@
 import * as vscode from 'vscode';
 
 export default class ElasticSearchResultsProvider implements vscode.TextDocumentContentProvider {
-  private results: Map<string, string>;
+  // private results: Map<string, string>;
+  public results: string;
+  
+  public queryResultsUri = vscode.Uri.parse("es-query://results");
+  private changeEvent = new vscode.EventEmitter<vscode.Uri>();
 
   public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string | Thenable<string> {
-    if (this.results.has(uri.path)) {
-      return this.results.get(uri.path);
-    } else {
-      return "No query found";
-    }
+    // if (this.results.has(uri.path)) {
+    //   return this.results.get(uri.path);
+    // } else {
+    //   return "No query found";
+    // }
+    return this.results;
   }
 
-  public addResults(queryKey: string, results: string) {
-    this.results.set(queryKey, results);
+  // public addResults(queryKey: string, results: string) {
+  //   this.results.set(queryKey, results);
+  // }
+
+  get onDidChange(): vscode.Event<vscode.Uri> {
+    return this.changeEvent.event;
   }
 
+  public update(results: string) {
+    this.results = results;
+    this.changeEvent.fire(this.queryResultsUri);
+  }
 }
